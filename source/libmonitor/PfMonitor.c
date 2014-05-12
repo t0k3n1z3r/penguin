d80bb0ae460ae60d06087dbb2f0996eabb0a1ead
m.koshel <m.koshel@samsung.com>
2014-05-12 17:11:39 +0300

    + libmonitor; + libprotocol; + libtransport libraries

---
+++ source/libmonitor/PfMonitor.c
@@ -0,0 +1,10 @@
+/**
+****************************************************************************************************
+* @file 	PfMonitor.c
+* @brief 	Penguin Framework [PF] process monitoring mechanisms implementation
+* @ingroup	PfMonitor
+* @author	Maksym Koshel (maks.koshel@gmail.com)
+* @date 	May 05, 2014
+* @copyright Maksym Koshel (c) 2014. All rights reserved.
+****************************************************************************************************
+*/
